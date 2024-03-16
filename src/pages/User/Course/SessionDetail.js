fb8ec66fc612c0b16d2cc99d3c0abaf0c4c1351f
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2024-03-16 16:53:45 -0400

    Update SessionDetail.js

--- src/pages/User/Course/SessionDetail.js
+++ src/pages/User/Course/SessionDetail.js
@@ -19,6 +19,39 @@ export const SessionDetail = () => {
     })
     .catch(error => console.error('Error fetching session details:', error));
   }, [sessionId]);
+ 
+  // 递归地渲染列表项
+  const renderListItems = (items) => {
+    return items.map((item, index) => {
+      if (item.type === 'list-item') {
+        return <li key={index} style={{ listStyleType: 'disc', marginLeft: '20px' }}>{renderListItems(item.children)}</li>;
+      }
+      return item.text;
+    });
+  };
+
+  // 渲染 objectives 中的每一项（可能是列表或段落）
+  const renderObjectivesItem = (item) => {
+    if (item.type === 'list') {
+      return item.format === 'unordered' ? <ul style={{ paddingLeft: 0 }}>{renderListItems(item.children)}</ul>
+                                         : <ol>{renderListItems(item.children)}</ol>;
+    } else if (item.type === 'paragraph') {
+      return <p>{item.children.map(child => child.text)}</p>;
+    }
+  };
+
+  // 渲染 objectives
+  const renderObjectives = (objectives) => {
+    return objectives.map((item, index) => renderObjectivesItem(item));
+  };
+
+
+  // 解析并渲染 overview
+  const renderOverview = (overview) => {
+    return overview.map((paragraph, index) => {
+      return <p key={index}>{paragraph.children.map(child => child.text)}</p>;
+    });
+  };
 
   if (!session) {
     return <div>Loading...</div>;
@@ -33,9 +66,16 @@ export const SessionDetail = () => {
       {/* 这里可以展示更多的content详情 */}
       <div style={{ backgroundColor: "#fff",borderRadius:10,padding:20,marginBottom:20 }}>
       <div style={{ fontWeight: "bold",marginBottom:10 }}>{session.attributes.Title}</div>
+      <div>
+          <strong>Overview:</strong>
+          {renderOverview(session.attributes.Overview)}
+        </div>
         <div>Tags: {session.attributes.Tags}</div>
         <div>Duration: {session.attributes.Duration}</div>
-        <div>Objective: {session.attributes.Objective}</div>
+        <div>
+          <strong>Objective:</strong>
+          <ul>{renderObjectives(session.attributes.Objectives)}</ul>
+        </div>
         <div>Audience: {session.attributes.Audience}</div>
       </div>
 
