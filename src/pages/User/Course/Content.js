d5f337f5f30b1423143b329150306f1dbc638054
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2024-04-04 17:02:34 -0400

    Update Content.js

--- src/pages/User/Course/Content.js
+++ src/pages/User/Course/Content.js
@@ -41,7 +41,7 @@ export const Content = ({ sessionId }) => {
                   src={`${content.attributes.Cover.data.attributes.url}`} 
                   alt={content.attributes.Title} 
                 />
-                <div className="px-6 py-4">
+                <div className="px-6 py-4" style={{ height: '150px', overflowY: 'auto' }}>
                   <div className="font-bold text-xl mb-2">{content.attributes.Type} - {content.attributes.Title}</div>
                   {/* 可以在这里添加一个简短的描述 */}
                   <p className="text-gray-700 text-base">
@@ -53,6 +53,7 @@ export const Content = ({ sessionId }) => {
                     <button 
                       className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
                       onClick={() => handleContentClick(content.id)}
+                      style={{ width: '120px' }}
                     >
                       <i className="far fa-eye"></i> Watch
                     </button>
@@ -61,8 +62,18 @@ export const Content = ({ sessionId }) => {
                     <button 
                       className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
                       onClick={() => handleContentClick(content.id)}
+                      style={{ width: '120px' }} 
                     >
                       <i className="fas fa-book"></i> Read
+                    </button>
+                  )}
+                  {content.attributes.Type === 'PowerPoint' && (
+                    <button 
+                      className="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
+                      onClick={() => handleContentClick(content.id)}
+                      style={{ width: '120px' }} 
+                    >
+                      <i className="fas fa-download"></i> Download
                     </button>
                   )}
                 </div>
