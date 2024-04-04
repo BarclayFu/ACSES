ed7b8fe75a0a4a554e8ac9a7a9c05e4ac38ba579
Pengfei <1519668478@qq.com>
2024-04-04 16:43:57 -0400

    hover

--- src/pages/User/Course/Session.js
+++ src/pages/User/Course/Session.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Session = ({ programId }) => {
   const [sessions, setSessions] = useState([]);
@@ -39,7 +40,7 @@ export const Session = ({ programId }) => {
               className="mb-2 p-4 transition duration-300 ease-in-out hover:shadow-lg hover:border-transparent border-b border-gray-200"
             >
               <li className="px-6 py-0 flex justify-between items-center">
-                <div className="w-1/3 text-sm font-medium text-blue-500" onClick={() => handleSessionClick(session.id, session.attributes.program.data.attributes.Title, session.attributes.Title)}>{session.attributes.Title}</div>
+                <Link className="w-1/3 text-sm font-medium text-blue-500" onClick={() => handleSessionClick(session.id, session.attributes.program.data.attributes.Title, session.attributes.Title)}>{session.attributes.Title}</Link>
                 <div className="w-1/3 text-sm font-medium text-gray-500 text-center">{session.attributes.Duration}</div>
                 <div className="w-1/3 text-sm font-medium text-gray-500 text-center">
                   {session.attributes.Tags ? (
